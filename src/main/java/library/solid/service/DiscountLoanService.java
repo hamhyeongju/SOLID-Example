967ddc829e9133e034376423ec47a18910e7ae92
hhj <gudwn357@gmail.com>
2023-01-15 17:19:09 +0900

    feat : create LoanServiceImpl

---
+++ src/main/java/library/solid/service/DiscountLoanService.java
@@ -0,0 +1,9 @@
+package library.solid.service;
+
+public class DiscountLoanService implements LoanService{
+
+    @Override
+    public void loan(Long memberId, Long bookId) {
+
+    }
+}
---
+++ src/main/java/library/solid/service/LimitLoanService.java
@@ -0,0 +1,9 @@
+package library.solid.service;
+
+public class LimitLoanService implements LoanService{
+
+    @Override
+    public void loan(Long memberId, Long bookId) {
+
+    }
+}
