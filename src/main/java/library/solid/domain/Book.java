68032c33bd3400055fde1500f8fb967131f4f05e
hhj <gudwn357@gmail.com>
2023-01-15 14:14:33 +0900

    feat : create Book

---
+++ src/main/java/library/solid/domain/Book.java
@@ -0,0 +1,11 @@
+package library.solid.domain;
+
+public class Book {
+
+    private Long id;
+    private String name;
+    private String author;
+    private int price;
+    private int stockQuantity;
+
+}
