b1a4ef8d382df5b650dcdd9e7fbb60613b34b5c3
hhj <gudwn357@gmail.com>
2023-01-16 16:16:15 +0900

    feat (Loan) : create create, return method

--- src/main/java/library/solid/domain/Loan.java
+++ src/main/java/library/solid/domain/Loan.java
@@ -1,10 +1,29 @@
 package library.solid.domain;
 
-import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
-@Getter @AllArgsConstructor
+@Getter @EqualsAndHashCode
 public class Loan {
 
     private Long id;
+    private Long bookId;
+    private int loanPrice;
+
+    public static Loan createLoan(Long id, Long bookId, int loanPrice, Member member, Book book) {
+        Loan loan = new Loan();
+        loan.id = id;
+        loan.bookId = bookId;
+        loan.loanPrice = loanPrice;
+
+        member.getLoans().put(loan.getId(), loan);
+        book.minusStockQuantity();
+
+        return loan;
+    }
+
+    public static void returnLoan(Long id, Member member, Book book) {
+        member.getLoans().remove(id);
+        book.plusStockQuantity();
+    }
 }
