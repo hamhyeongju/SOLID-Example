a083eb7fcfa275d9e272e68960927d662de93a83
hhj <gudwn357@gmail.com>
2023-01-16 16:37:08 +0900

    feat (LimitLoanService) : override methdod

--- src/main/java/library/solid/service/LimitLoanService.java
+++ src/main/java/library/solid/service/LimitLoanService.java
@@ -1,9 +1,46 @@
 package library.solid.service;
 
+import library.solid.domain.*;
+import library.solid.exception.OutOfLoanLimitException;
+import library.solid.exception.OutOfStockException;
+import library.solid.repository.BookRepository;
+import library.solid.repository.MemberRepository;
+
+/**
+ * BASIC - 최대 1권 대출 가능
+ * VIP - 최대 3권 대출 가능
+ *
+ * 공통 정책 - 책 가격의 10% 요금
+ */
 public class LimitLoanService implements LoanService{
 
-    @Override
-    public void loan(Long memberId, Long bookId) {
+    private final MemberRepository memberRepository;
+    private final BookRepository bookRepository;
 
+    public LimitLoanService(MemberRepository memberRepository, BookRepository bookRepository) {
+        this.memberRepository = memberRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Loan loan(Long memberId, Long bookId) {
+        Member member = memberRepository.findById(memberId);
+        Book book = bookRepository.findById(bookId);
+
+        if (book.getStockQuantity() == 0) throw new OutOfStockException();
+        if (member.getGrade().equals(Grade.BASIC)) {
+            if (member.getLoans().size() >= 1) throw new OutOfLoanLimitException();
+        } else {
+            if (member.getLoans().size() >= 3) throw new OutOfLoanLimitException();
+        }
+
+        return Loan.createLoan(Sequence.getSequence(), bookId, book.getPrice() / 10, member, book);
+    }
+
+    @Override
+    public void returnBook(Long loanId, Long memberId, Long bookId) {
+        Member member = memberRepository.findById(memberId);
+        Book book = bookRepository.findById(bookId);
+        Loan.returnLoan(loanId, member, book);
     }
 }
