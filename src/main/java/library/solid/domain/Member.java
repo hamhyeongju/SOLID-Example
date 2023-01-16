72f383af04f9559d2b4898fd731d9055d1160b36
hhj <gudwn357@gmail.com>
2023-01-16 15:46:21 +0900

    feat (Member) : add loans field

--- src/main/java/library/solid/domain/Member.java
+++ src/main/java/library/solid/domain/Member.java
@@ -3,6 +3,8 @@ package library.solid.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Map;
+
 @Getter
 @AllArgsConstructor
 public class Member {
@@ -10,5 +12,6 @@ public class Member {
     private Long id;
     private String name;
     private Grade grade;
+    private Map<Long, Loan> loans;
 
 }
