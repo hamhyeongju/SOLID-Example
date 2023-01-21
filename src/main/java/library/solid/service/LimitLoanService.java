6a67fdcaf12bb3bec95de25f279e65b5f04a8262
hhj <gudwn357@gmail.com>
2023-01-21 19:33:16 +0900

    refactor (LimitLoanService) : edit Loan.createLoan() parameters, add loanRepository.delete in returnBook()

--- src/main/java/library/solid/service/LimitLoanService.java
+++ src/main/java/library/solid/service/LimitLoanService.java
@@ -4,6 +4,7 @@ import library.solid.domain.*;
 import library.solid.exception.OutOfLoanLimitException;
 import library.solid.exception.OutOfStockException;
 import library.solid.repository.BookRepository;
+import library.solid.repository.LoanRepository;
 import library.solid.repository.MemberRepository;
 
 /**
@@ -16,10 +17,12 @@ public class LimitLoanService implements LoanService{
 
     private final MemberRepository memberRepository;
     private final BookRepository bookRepository;
+    private final LoanRepository loanRepository;
 
-    public LimitLoanService(MemberRepository memberRepository, BookRepository bookRepository) {
+    public LimitLoanService(MemberRepository memberRepository, BookRepository bookRepository, LoanRepository loanRepository) {
         this.memberRepository = memberRepository;
         this.bookRepository = bookRepository;
+        this.loanRepository = loanRepository;
     }
 
     @Override
@@ -33,13 +36,11 @@ public class LimitLoanService implements LoanService{
         else if (member.getGrade().equals(Grade.VIP) && member.getLoans().size() >= 3)
             throw new OutOfLoanLimitException();
 
-        return Loan.createLoan(Sequence.getSequence(), bookId, book.getPrice() / 10, member, book);
+        return Loan.createLoan(Sequence.getSequence(), book.getPrice() / 10, member, book);
     }
 
     @Override
-    public void returnBook(Long loanId, Long memberId, Long bookId) {
-        Member member = memberRepository.findById(memberId);
-        Book book = bookRepository.findById(bookId);
-        Loan.returnLoan(loanId, member, book);
+    public void returnBook(Loan loan) {
+        loanRepository.delete(loan);
     }
 }
